d2bc75ea8a32f912a3c993959df50fdd428c028f
griffinbentley <47612646+griffinbentley@users.noreply.github.com>
2019-10-28 02:02:24 -0700

    Added comments and removed unecessary code
    Added comments and got rid of declaration of function that was the only thing that got called in this file.

--- columns.py
+++ columns.py
@@ -3,33 +3,26 @@ from settings import Settings
 import game_logic as gl
 from pygame.sprite import Group
 
-def run_game():
-    pygame.init()
-    settings = Settings()
-    screen = pygame.display.set_mode((settings.screen_width,settings.screen_height))
-    pygame.display.set_caption("Columns")
+# Initializes variables used in running the game.
+pygame.init()
+settings = Settings()
+screen = pygame.display.set_mode((settings.screen_width,settings.screen_height))
+pygame.display.set_caption("Columns")
+lose = False
+column = Group()
+board = [[0] * 6 for i in xrange(12)]
+gl.create_column(settings,screen,column)
+score = [0]
 
-    lose = False
+# Runs the game while the user hasn't lost.
+while not lose:
+    if gl.check_events(column,board):
+        run_game()
+    gl.update_screen(settings,screen,column,board,score)
+    gl.update_column(settings,screen,column,board,score)
+    lose = gl.check_state(board,screen)
 
-    column = Group()
-
-    board = [[0] * 6 for i in xrange(12)]
-
-    gl.create_column(settings,screen,column)
-
-    score = [0]
-
-    while not lose:
-        if gl.check_events(column,board):
-            run_game()
-        gl.update_screen(settings,screen,column,board,score)
-        gl.update_column(settings,screen,column,board,score)
-        lose = gl.check_state(board,screen)
-
-    if lose:
-        while True:
-            if gl.check_events(column,board):
-                run_game()
-            gl.lose_screen(settings,screen)
-
-run_game()
+# Displays the end screen until the user closes the program.
+while True:
+    gl.check_events(column,board)
+    gl.lose_screen(settings,screen)
