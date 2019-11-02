78f23748385b388b91bcb9200382ad63106f9332
griffinbentley <47612646+griffinbentley@users.noreply.github.com>
2019-11-02 16:31:19 -0700

    Added wait time between actions
    Changed check_matches() and move_down() so that they now work on lists of blocks instead of single blocks. Added a wait time in between their actions so it's easier to understand what's happening and why. Added screen updates in both functions to illustrate the wait times better. Also changed the way they're called in add_column() so that they operate on on blocks in the right order.

--- game_logic.py
+++ game_logic.py
@@ -1,21 +1,21 @@
 import pygame
 import random
 from block import Block
+from pygame.sprite import Group
 
 # Checks to see if there were any button events.
-def check_events(column,board):
+def check_events(column,board,pause):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
         if event.type == pygame.KEYDOWN:
-            if check_keydown_events(event,column,board):
-                return True
+            check_keydown_events(event,column,board,pause)
         if event.type == pygame.KEYUP:
             check_keyup_events(event,column)
 
 # Checks to see if any buttons were pressed.
-def check_keydown_events(event,column,board):
+def check_keydown_events(event,column,board,pause):
     if event.key == pygame.K_RIGHT:
         for n in range(2,-1,-1):
             x = column.sprites()[n].rect.centerx
@@ -36,10 +36,13 @@ def check_keydown_events(event,column,board):
     elif event.key == pygame.K_q:
         pygame.quit()
         quit()
-    elif event.key == pygame.K_r:
-        return True
     elif event.key == pygame.K_SPACE:
         swap(column)
+    elif event.key == pygame.K_ESCAPE:
+        if pause[0]:
+            pause[0] = False
+        else:
+            pause[0] = True
 
 # Checks to see if any buttons were released.
 def check_keyup_events(event,column):
@@ -77,88 +80,97 @@ def create_block(settings,screen,column,y,x):
     column.add(block)
 
 # Adds the given column to the board and checks matches.
-def add_to_board(column,board,score):
+def add_to_board(settings, screen, column,board,score):
     for block in column:
         block.board_x = (block.rect.x - 255) / 40
         block.board_y = (block.rect.y * 11 - 1485) / 440
         board[block.board_y][block.board_x] = block
+    columns = []
+    blocks = []
     for block in column:
-        check_matches(block, board, score)
+        blocks.append(block)
+    columns = check_matches(settings, screen, blocks, board, score)
+    blocks = move_down(settings, screen, columns, board, score)
+    while len(blocks) != 0:
+        columns = check_matches(settings, screen, blocks, board, score)
+        blocks = move_down(settings, screen, columns, board, score)
     column.empty()
 
 # Checks to see if the given block has any matches in its vicinity.
-def check_matches(block, board, score):
+def check_matches(settings, screen, blocks, board, score):
     to_remove = [[]]
-    x = block.board_x
-    y = block.board_y
+    # Checks matches for each block in the array of blocks.
+    for block in blocks:
+        x = block.board_x
+        y = block.board_y
 
-    # Checks horizontally.
-    h_remove = [block]
-    if x >= 1 and isinstance(board[y][x - 1], Block):
-        if board[y][x - 1].color == block.color:
-            h_remove.append(board[y][x - 1])
-            if x >= 2 and isinstance(board[y][x - 2], Block):
-                if board[y][x - 2].color == block.color:
-                    h_remove.append(board[y][x - 2])
-    if x <= 4 and isinstance(board[y][x + 1], Block):
-        if board[y][x + 1].color == block.color:
-            h_remove.append(board[y][x + 1])
-            if x <= 3 and isinstance(board[y][x + 2], Block):
-                if board[y][x + 2].color == block.color:
-                    h_remove.append(board[y][x + 2])
-    if len(h_remove) >= 3:
-        to_remove.append(h_remove)
+        # Checks horizontally.
+        h_remove = [block]
+        if x >= 1 and isinstance(board[y][x - 1], Block):
+            if board[y][x - 1].color == block.color:
+                h_remove.append(board[y][x - 1])
+                if x >= 2 and isinstance(board[y][x - 2], Block):
+                    if board[y][x - 2].color == block.color:
+                        h_remove.append(board[y][x - 2])
+        if x <= 4 and isinstance(board[y][x + 1], Block):
+            if board[y][x + 1].color == block.color:
+                h_remove.append(board[y][x + 1])
+                if x <= 3 and isinstance(board[y][x + 2], Block):
+                    if board[y][x + 2].color == block.color:
+                        h_remove.append(board[y][x + 2])
+        if len(h_remove) >= 3:
+            to_remove.append(h_remove)
 
-    # Checks vertically.
-    v_remove = [block]
-    if y >= 1 and isinstance(board[y - 1][x], Block):
-        if board[y - 1][x].color == block.color:
-            v_remove.append(board[y - 1][x])
-            if y >= 2 and isinstance(board[y - 2][x], Block):
-                if board[y - 2][x].color == block.color:
-                    v_remove.append(board[y - 2][x])
-    if y <= 10 and isinstance(board[y + 1][x], Block):
-        if board[y + 1][x].color == block.color:
-            v_remove.append(board[y + 1][x])
-            if y <= 9 and isinstance(board[y + 2][x], Block):
-                if board[y + 2][x].color == block.color:
-                    v_remove.append(board[y + 2][x])
-    if len(v_remove) >= 3:
-        to_remove.append(v_remove)
+        # Checks vertically.
+        v_remove = [block]
+        if y >= 1 and isinstance(board[y - 1][x], Block):
+            if board[y - 1][x].color == block.color:
+                v_remove.append(board[y - 1][x])
+                if y >= 2 and isinstance(board[y - 2][x], Block):
+                    if board[y - 2][x].color == block.color:
+                        v_remove.append(board[y - 2][x])
+        if y <= 10 and isinstance(board[y + 1][x], Block):
+            if board[y + 1][x].color == block.color:
+                v_remove.append(board[y + 1][x])
+                if y <= 9 and isinstance(board[y + 2][x], Block):
+                    if board[y + 2][x].color == block.color:
+                        v_remove.append(board[y + 2][x])
+        if len(v_remove) >= 3:
+            to_remove.append(v_remove)
 
-    # Checks angle top left to bottom right.
-    ua_remove = [block]
-    if x >= 1 and y >= 1 and isinstance(board[y - 1][x - 1], Block):
-        if board[y - 1][x - 1].color == block.color:
-            ua_remove.append(board[y - 1][x - 1])
-            if x >= 2 and y >= 2 and isinstance(board[y - 2][x - 2], Block):
-                if board[y - 2][x - 2].color == block.color:
-                    ua_remove.append(board[y - 2][x - 2])
-    if x <= 4 and y <= 10 and isinstance(board[y + 1][x + 1], Block):
-        if board[y + 1][x + 1].color == block.color:
-            ua_remove.append(board[y + 1][x + 1])
-            if x <= 3 and y <= 9 and isinstance(board[y + 2][x + 2], Block):
-                if board[y + 2][x + 2].color == block.color:
-                    ua_remove.append(board[y + 2][x + 2])
-    if len(ua_remove) >= 3:
-        to_remove.append(ua_remove)
+        # Checks angle top left to bottom right.
+        ua_remove = [block]
+        if x >= 1 and y >= 1 and isinstance(board[y - 1][x - 1], Block):
+            if board[y - 1][x - 1].color == block.color:
+                ua_remove.append(board[y - 1][x - 1])
+                if x >= 2 and y >= 2 and isinstance(board[y - 2][x - 2], Block):
+                    if board[y - 2][x - 2].color == block.color:
+                        ua_remove.append(board[y - 2][x - 2])
+        if x <= 4 and y <= 10 and isinstance(board[y + 1][x + 1], Block):
+            if board[y + 1][x + 1].color == block.color:
+                ua_remove.append(board[y + 1][x + 1])
+                if x <= 3 and y <= 9 and isinstance(board[y + 2][x + 2], Block):
+                    if board[y + 2][x + 2].color == block.color:
+                        ua_remove.append(board[y + 2][x + 2])
+        if len(ua_remove) >= 3:
+            to_remove.append(ua_remove)
 
-    # Checks angle bottom left to top right.
-    da_remove = [block]
-    if x >= 1 and y <= 10 and isinstance(board[y + 1][x - 1], Block):
-        if board[y + 1][x - 1].color == block.color:
-            da_remove.append(board[y + 1][x - 1])
-            if x >= 2 and y <= 9 and isinstance(board[y +2][x - 2], Block):
-                if board[y + 2][x - 2].color == block.color:
-                    da_remove.append(board[y +2][x - 2])
-    if x <= 4 and y >= 1 and isinstance(board[y - 1][x + 1], Block):
-        if board[y - 1][x + 1].color == block.color:
-            da_remove.append(board[y - 1][x + 1])
-            if x <= 3 and y >= 2 and isinstance(board[y - 2][x + 2], Block):
-                if board[y - 2][x + 2].color == block.color:
-                    da_remove.append(board[y - 2][x + 2])
-    if len(da_remove) >= 3:
-        to_remove.append(da_remove)
+        # Checks angle bottom left to top right.
+        da_remove = [block]
+        if x >= 1 and y <= 10 and isinstance(board[y + 1][x - 1], Block):
+            if board[y + 1][x - 1].color == block.color:
+                da_remove.append(board[y + 1][x - 1])
+                if x >= 2 and y <= 9 and isinstance(board[y +2][x - 2], Block):
+                    if board[y + 2][x - 2].color == block.color:
+                        da_remove.append(board[y +2][x - 2])
+        if x <= 4 and y >= 1 and isinstance(board[y - 1][x + 1], Block):
+            if board[y - 1][x + 1].color == block.color:
+                da_remove.append(board[y - 1][x + 1])
+                if x <= 3 and y >= 2 and isinstance(board[y - 2][x + 2], Block):
+                    if board[y - 2][x + 2].color == block.color:
+                        da_remove.append(board[y - 2][x + 2])
+        if len(da_remove) >= 3:
+            to_remove.append(da_remove)
 
     # Stores columns that need to be moved down after removal.
     cols = set()
@@ -170,10 +182,10 @@ def check_matches(block, board, score):
             num_remove += 1
             board[block.board_y][block.board_x] = 0
             cols.add(block.board_x)
-
-    # Moves down columns where blocks were removed.
-    for column in cols:
-        move_down(column, board, score)
+    column = Group()
+    update_screen(settings, screen, column, board, score)
+    if len(remove) != 0:
+        pygame.time.wait(1000)
 
     # Adds to score based on the number of blocks removed.
     if num_remove >= 5:
@@ -183,23 +195,30 @@ def check_matches(block, board, score):
     elif num_remove == 3:
         score[0] += num_remove * 500
 
+    # Returns the cols so that they can be checked in move_down.
+    return cols
+
 # Moves down all blocks with no blocks under them in the given column.
-def move_down(x, board, score):
+def move_down(settings, screen, columns, board, score):
     blocks = set()
-    y = 10
-    while y != -1:
-        if y != 11 and board[y][x] != 0 and board[y + 1][x] == 0:
-            blocks.add(board[y][x])
-            board[y][x].rect.y += board[y][x].width
-            board[y][x].y = board[y][x].rect.y
-            board[y][x].board_y += 1
-            board[y + 1][x] = board[y][x]
-            board[y][x] = 0
-            y += 1
-        else:
-            y -= 1
-    for block in blocks:
-        check_matches(block, board, score)
+    for x in columns:
+        y = 10
+        while y != -1:
+            if y != 11 and board[y][x] != 0 and board[y + 1][x] == 0:
+                blocks.add(board[y][x])
+                board[y][x].rect.y += board[y][x].width
+                board[y][x].y = board[y][x].rect.y
+                board[y][x].board_y += 1
+                board[y + 1][x] = board[y][x]
+                board[y][x] = 0
+                y += 1
+            else:
+                y -= 1
+    column = Group()
+    if len(blocks) != 0:
+        update_screen(settings, screen, column, board, score)
+        pygame.time.wait(1000)
+    return blocks
 
 # Moves the columns down the screen.
 def update_column(settings,screen,column,board,score):
@@ -211,7 +230,7 @@ def update_column(settings,screen,column,board,score):
         if lowest.check_bottom(board):
             column.update()
         else:
-            add_to_board(column,board,score)
+            add_to_board(settings, screen, column, board, score)
             create_column(settings,screen,column)
 
 # Checks all blocks to see if one has been placed on top of the playfield and if so
@@ -230,16 +249,25 @@ def draw_grid(screen,settings):
     rightx = leftx + settings.block_width*6
     topy = screen.get_rect().centery - settings.block_width*6
     bottomy = topy + settings.block_width*12
-    for x in range(0,13):
-        pygame.draw.line(screen,(160,160,160),(leftx,topy+x*settings.block_width),(rightx,topy+x*settings.block_width),5)
-    for x in range(0,7):
-        pygame.draw.line(screen,(160,160,160),(leftx+x*settings.block_width,topy),(leftx+x*settings.block_width,bottomy),5)
+    for x in range(0, 13):
+        pygame.draw.line(screen, settings.grid_color, (leftx, topy+x*settings.block_width),
+                         (rightx, topy+x*settings.block_width), 5)
+    for x in range(0, 7):
+        pygame.draw.line(screen, settings.grid_color, (leftx+x*settings.block_width, topy),
+                                  (leftx+x*settings.block_width, bottomy), 5)
 
 # Displays "YOU LOSE" text at the end of the game.
 def lose_screen(settings,screen):
     font = pygame.font.Font(None, 100)
-    text = font.render('YOU LOSE', True, (0,0,0))
-    screen.blit(text, (settings.screen_width/2-text.get_rect().centerx,settings.screen_height/2-text.get_rect().centery))
+    text = font.render('YOU LOSE', True, settings.lose_color)
+    screen.blit(text, (settings.screen_width/2-text.get_rect().centerx, settings.screen_height/2-text.get_rect().centery))
+    pygame.display.flip()
+
+# Displays "PAUSED" when the game is paused.
+def pause_screen(settings,screen):
+    font = pygame.font.Font(None, 100)
+    text = font.render('PAUSED', True, settings.lose_color)
+    screen.blit(text, (settings.screen_width/2-text.get_rect().centerx, settings.screen_height/2-text.get_rect().centery))
     pygame.display.flip()
 
 # Displays the player's score at the top of the window.
