a2096b24a74ae3f53448899f97bb975e72d2b56e
griffinbentley <47612646+griffinbentley@users.noreply.github.com>
2019-11-02 16:34:32 -0700

    Added pause functionality
    Added pause variable and made it so that while pause is true the game stops running.

--- columns.py
+++ columns.py
@@ -9,6 +9,7 @@ settings = Settings()
 screen = pygame.display.set_mode((settings.screen_width,settings.screen_height))
 pygame.display.set_caption("Columns")
 lose = False
+pause = [False]
 column = Group()
 board = [[0] * 6 for i in xrange(12)]
 gl.create_column(settings,screen,column)
@@ -16,13 +17,15 @@ score = [0]
 
 # Runs the game while the user hasn't lost.
 while not lose:
-    if gl.check_events(column,board):
-        run_game()
-    gl.update_screen(settings,screen,column,board,score)
-    gl.update_column(settings,screen,column,board,score)
-    lose = gl.check_state(board,screen)
+    gl.check_events(column,board,pause)
+    if not pause[0]:
+        gl.update_screen(settings,screen,column,board,score)
+        gl.update_column(settings,screen,column,board,score)
+        lose = gl.check_state(board,screen)
+    else:
+        gl.pause_screen(settings, screen)
 
 # Displays the end screen until the user closes the program.
 while True:
-    gl.check_events(column,board)
     gl.lose_screen(settings,screen)
+    gl.check_events(column,board,pause)
